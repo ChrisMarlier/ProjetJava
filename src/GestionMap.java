1040cd3b6790962f89687711bdd371296b33c273
raph <r.teruel997@gmail.com>
2019-04-26 17:15:28 +0200

    calcule des déplacement possible

--- src/GestionMap.java
+++ src/GestionMap.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 public class GestionMap {
 
 	ArrayList<ArrayList<Integer>> map = new ArrayList<ArrayList<Integer>>();
@@ -32,5 +31,66 @@ public class GestionMap {
 	            e.printStackTrace();
 	        }
 
+	}
+
+
+	public void calculeDéplacementValide(Unité unité){
+
+		ArrayList<ArrayList<Integer>> test = new ArrayList<ArrayList<Integer>>();
+
+
+		for(int i=0;i<20;i++){
+			ArrayList<Integer> ListInt = new ArrayList<Integer>();
+			for(int j=0;j<24;j++){
+				ListInt.add(0);
+			}
+			test.add(ListInt);
+		}
+		Héxagone héxagone=new Héxagone(unité.getCoordonnéeI(),unité.getCoordonnééJ());
+		test=calcule(test,unité.getPtDep()+1,héxagone);
+
+		for(int i=0;i<20;i++){
+			System.out.println(test.get(i));
+		}
+
+	}
+	private ArrayList<ArrayList<Integer>> calcule(ArrayList<ArrayList<Integer>> test, int ptDep, Héxagone héxagone){
+
+
+		if( ptDep>0){
+			test.get(héxagone.getI()).remove(héxagone.getJ());
+			test.get(héxagone.getI()).add(héxagone.getJ(),1);
+			ptDep-=map.get(héxagone.getI()).get(héxagone.getJ());
+			System.out.println(ptDep+""+héxagone.getI()+""+héxagone.getJ()+"\n");
+
+
+
+			if(héxagone.getI()>0){
+				test=calcule(test, ptDep,héxagone.voisinHaut());
+			}
+			if(héxagone.getI()<19){
+				test=calcule(test, ptDep,héxagone.voisinBah());
+			}
+
+
+//
+			if (héxagone.getJ() > 0 && héxagone.getI() > 0) {
+				test = calcule(test, ptDep,héxagone.voisinHautGauche());
+			}
+			if (héxagone.getJ() < 24 && héxagone.getI() > 0) {
+				test = calcule(test, ptDep, héxagone.voisinHautdroit());
+			}
+
+			if ((héxagone.getJ() > 0 && héxagone.getI() < 19) || (héxagone.getI() == 19)) {
+				test = calcule(test, ptDep,héxagone.voisinBahGauche());
+			}
+			if ((héxagone.getJ() < 24 && héxagone.getI() < 19) || (héxagone.getI() == 19 )) {
+				test = calcule(test, ptDep, héxagone.voisinBahDroit());
+			}
+			}
+
+
+
+		return test;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ public class Main {
 
 		GestionMap test = new GestionMap();
 		test.chargementMap();
-		
+
+		Unité unité=new Unité(10,10,5,5,10,10,10,0,3);
+		test.calculeDéplacementValide(unité);
 	}
 
 }
